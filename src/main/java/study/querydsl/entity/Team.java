e1476ce7e9c98f831afb033a7bc3fddf4646b845
1ssgk <must9126@gmail.com>
2023-03-22 17:42:13 +0900

    엔티티들

---
+++ src/main/java/study/querydsl/entity/Team.java
@@ -0,0 +1,35 @@
+package study.querydsl.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "name"})
+public class Team {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "team") // 연관 관계에 있어서 주인이 아님을 표시
+    private List<Member> members = new ArrayList();
+
+    public Team(String name) {
+        this.name = name;
+    }
+}
