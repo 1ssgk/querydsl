e295eb5990df07a31a9ff5276804978f82f95a2b
1ssgk <must9126@gmail.com>
2023-03-23 23:12:48 +0900

    인터페이스 지원 - QuerydslPredicateExecutor
    JPA에서 querydsl 기능을 지원.
    하지만 테이블이 join 되는 경우 사용이 거의 불가능.
    현업에서 사용은 비추

--- src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Pageable;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
@@ -17,6 +18,7 @@ import javax.transaction.Transactional;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static study.querydsl.entity.QMember.*;
 
 @SpringBootTest
 @Transactional
@@ -29,8 +31,8 @@ class MemberRepositoryTest {
     EntityManager em;
 
     @Test
-    public void basicTest(){
-        Member member = new Member("member1",10);
+    public void basicTest() {
+        Member member = new Member("member1", 10);
         memberRepository.save(member);
 
         Member findMember = memberRepository.findById(member.getId()).get();
@@ -44,7 +46,7 @@ class MemberRepositoryTest {
     }
 
     @Test
-    public void searchTest(){
+    public void searchTest() {
         Team teamA = new Team("teamA");
         Team teamB = new Team("teamB");
         em.persist(teamA);
@@ -73,7 +75,7 @@ class MemberRepositoryTest {
     }
 
     @Test
-    public void searchPageSimple(){
+    public void searchPageSimple() {
         Team teamA = new Team("teamA");
         Team teamB = new Team("teamB");
         em.persist(teamA);
@@ -96,7 +98,7 @@ class MemberRepositoryTest {
 
         PageRequest pageRequest = PageRequest.of(0, 3);
 
-        Page<MemberTeamDto> result = memberRepository.searchPageComplex(condition,pageRequest);
+        Page<MemberTeamDto> result = memberRepository.searchPageComplex(condition, pageRequest);
 
         assertThat(result.getSize()).isEqualTo(3);
         assertThat(result.getContent())
@@ -105,4 +107,31 @@ class MemberRepositoryTest {
 
     }
 
+
+    @Test
+    public void querydslPredicateExecutorTest() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        Iterable<Member> result = memberRepository.findAll(
+                member.age.between(10, 40)
+                        .and(member.username.eq("member1"))
+        );
+
+        for (Member findMember : result) {
+            System.out.println("findMember = " + findMember);
+        }
+    }
 }
